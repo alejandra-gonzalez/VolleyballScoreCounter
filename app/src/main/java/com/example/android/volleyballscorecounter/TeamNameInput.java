6b1bc8041cac3b4d6817b42805d8a982be003f41
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-05-12 20:48:37 -0700

    XML for Launch Screen

---
+++ app/src/main/java/com/example/android/volleyballscorecounter/TeamNameInput.java
@@ -0,0 +1,13 @@
+package com.example.android.volleyballscorecounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TeamNameInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_name_input);
+    }
+}
